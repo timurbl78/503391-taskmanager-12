fbbada8c5c04b58525533bf73cb11cde75040844
Тимур Белов <timurbl574@gmail.com>
2020-08-23 19:03:02 +0300

    favorite & archive Handlers

--- src/view/task.js
+++ src/view/task.js
@@ -7,6 +7,8 @@ export default class Task extends AbstractView {
     this._task = task;
 
     this._editClickHandler = this._editClickHandler.bind(this);
+    this._favoriteClickHandler = this._favoriteClickHandler.bind(this);
+    this._archiveClickHandler = this._archiveClickHandler.bind(this);
   }
 
   _editClickHandler(evt) {
@@ -14,13 +16,33 @@ export default class Task extends AbstractView {
     this._callback.editClick();
   }
 
+  _favoriteClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.favoriteClick();
+  }
+
+  _archiveClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.archiveClick();
+  }
+
   setEditClickHandler(callback) {
     this._callback.editClick = callback;
     this.getElement().querySelector(`.card__btn--edit`).addEventListener(`click`, this._editClickHandler);
   }
 
+  setFavoriteClickHandler(callback) {
+    this._callback.favoriteClick = callback;
+    this.getElement().querySelector(`.card__btn--favorites`).addEventListener(`click`, this._favoriteClickHandler);
+  }
+
+  setArchiveClickHandler(callback) {
+    this._callback.archiveClick = callback;
+    this.getElement().querySelector(`.card__btn--archive`).addEventListener(`click`, this._archiveClickHandler);
+  }
+
   _createTaskTemplate(task) {
-    const {color, description, dueDate, repeatingDays, isArchive, isFavorite} = task;
+    const {color, description, dueDate, repeating, isArchive, isFavorite} = task;
 
     const date = dueDate !== null
       ? humanizeTaskDueDate(dueDate)
@@ -30,7 +52,7 @@ export default class Task extends AbstractView {
       ? `card--deadline`
       : ``;
 
-    const repeatClassName = isTaskRepeating(repeatingDays)
+    const repeatClassName = isTaskRepeating(repeating)
       ? `card--repeat`
       : ``;
 
