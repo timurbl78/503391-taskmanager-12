1c63f01ed3af8d90be420be8ceff58b03102ab2e
Тимур Белов <timurbl574@gmail.com>
2020-08-29 23:54:36 +0300

    typo

--- src/view/sort.js
+++ src/view/sort.js
@@ -24,10 +24,10 @@ export default class Sort extends AbstractView {
 
   _createSortTemplate() {
     return (`<div class="board__filter-list">
-    <a href="#" class="board__filter data-sort-type="${SortType.DEFAULT}">SORT BY DEFAULT</a>
-    <a href="#" class="board__filter data-sort-type="${SortType.DATE_UP}">SORT BY DATE up</a>
-    <a href="#" class="board__filter data-sort-type="${SortType.DATE_DOWN}">SORT BY DATE down</a>
-  </div>`);
+      <a href="#" class="board__filter" data-sort-type="${SortType.DEFAULT}">SORT BY DEFAULT</a>
+      <a href="#" class="board__filter" data-sort-type="${SortType.DATE_UP}">SORT BY DATE up</a>
+      <a href="#" class="board__filter" data-sort-type="${SortType.DATE_DOWN}">SORT BY DATE down</a>
+    </div>`);
   }
 
   _getTemplate() {
