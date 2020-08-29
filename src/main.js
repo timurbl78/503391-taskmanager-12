fd1e6092e62709ac211200b2fd38497d56a34eee
Тимур Белов <timurbl574@gmail.com>
2020-08-30 00:36:59 +0300

    tasks model

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import MainMenuView from "./view/main-menu";
 import BoardPresenter from "./presenter/board";
 import {generateTask} from "./mock/task";
 import {generateFilter} from "./mock/filter";
+import TasksModel from "./model/tasks.js";
 import {render, RenderPosition} from "./utils/render";
 
 const TASK_COUNT = 20;
@@ -10,12 +11,15 @@ const TASK_COUNT = 20;
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 const filters = generateFilter(tasks);
 
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
+
 const siteMainElement = document.querySelector(`.main`);
 const siteControlElement = siteMainElement.querySelector(`.main__control`);
 
-const boardPresenter = new BoardPresenter(siteMainElement);
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
 
 render(siteControlElement, new MainMenuView(), RenderPosition.BEFOREEND);
 render(siteMainElement, new FiltersView(filters), RenderPosition.BEFOREEND);
 
-boardPresenter.init(tasks);
+boardPresenter.init();
