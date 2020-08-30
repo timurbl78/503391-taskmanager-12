7a63ac70ade91b3ea3a436c7b1b56c52a9c6ec76
Тимур Белов <timurbl574@gmail.com>
2020-08-31 01:09:34 +0300

    statictics block

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import MainMenuView from "./view/main-menu";
+import StatisticsView from "./view/statistics.js";
 import BoardPresenter from "./presenter/board";
 import FilterPresenter from "./presenter/filter.js";
 import {generateTask} from "./mock/task";
 import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
-import {render, RenderPosition} from "./utils/render";
+import {render, RenderPosition, remove} from "./utils/render";
+import {MenuItem, UpdateType, FilterType} from "./const.js";
 
 const TASK_COUNT = 20;
 
@@ -18,15 +20,41 @@ const filterModel = new FilterModel();
 const siteMainElement = document.querySelector(`.main`);
 const siteControlElement = siteMainElement.querySelector(`.main__control`);
 
-render(siteControlElement, new MainMenuView(), RenderPosition.BEFOREEND);
+const mainMenuComponent = new MainMenuView();
+render(siteControlElement, mainMenuComponent, RenderPosition.BEFOREEND);
 
 const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel);
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
 
+const handleTaskNewFormClose = () => {
+  mainMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = false;
+  mainMenuComponent.setMenuItem(MenuItem.TASKS);
+};
+
+let statisticsComponent = null;
+
+const handleSiteMenuClick = (menuItem) => {
+  switch (menuItem) {
+    case MenuItem.ADD_NEW_TASK:
+      remove(statisticsComponent);
+      boardPresenter.destroy();
+      filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
+      boardPresenter.init();
+      boardPresenter.createTask(handleTaskNewFormClose);
+      mainMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = true;
+      break;
+    case MenuItem.TASKS:
+      boardPresenter.init();
+      remove(statisticsComponent);
+      break;
+    case MenuItem.STATISTICS:
+      boardPresenter.destroy();
+      statisticsComponent = new StatisticsView(tasksModel.getTasks());
+      render(siteMainElement, statisticsComponent, RenderPosition.BEFOREEND);
+      break;
+  }
+};
+
+mainMenuComponent.setMenuClickHandler(handleSiteMenuClick);
 filterPresenter.init();
 boardPresenter.init();
-
-document.querySelector(`#control__new-task`).addEventListener(`click`, (evt) => {
-  evt.preventDefault();
-  boardPresenter.createTask();
-});
--- src/view/main-menu.js
+++ src/view/main-menu.js
@@ -1,6 +1,31 @@
 import AbstractView from "./abstract";
+import {MenuItem} from "../const.js";
 
 export default class MainMenu extends AbstractView {
+  constructor() {
+    super();
+
+    this._menuClickHandler = this._menuClickHandler.bind(this);
+  }
+
+  _menuClickHandler(evt) {
+    evt.preventDefault();
+    this._callback.menuClick(evt.target.value);
+  }
+
+  setMenuClickHandler(callback) {
+    this._callback.menuClick = callback;
+    this.getElement().addEventListener(`change`, this._menuClickHandler);
+  }
+
+  setMenuItem(menuItem) {
+    const item = this.getElement().querySelector(`[value=${menuItem}]`);
+
+    if (item !== null) {
+      item.checked = true;
+    }
+  }
+
   _createMainMenuTemplate() {
     return (
       `<section class="control__btn-wrap">
@@ -9,6 +34,7 @@ export default class MainMenu extends AbstractView {
             name="control"
             id="control__new-task"
             class="control__input visually-hidden"
+            value="${MenuItem.ADD_NEW_TASK}"
           />
           <label for="control__new-task" class="control__label control__label--new-task"
             >+ ADD NEW TASK</label
@@ -18,6 +44,7 @@ export default class MainMenu extends AbstractView {
             name="control"
             id="control__task"
             class="control__input visually-hidden"
+            value="${MenuItem.TASKS}"
             checked
           />
           <label for="control__task" class="control__label">TASKS</label>
@@ -26,6 +53,7 @@ export default class MainMenu extends AbstractView {
             name="control"
             id="control__statistic"
             class="control__input visually-hidden"
+            value="${MenuItem.STATISTICS}"
           />
           <label for="control__statistic" class="control__label"
             >STATISTICS</label
